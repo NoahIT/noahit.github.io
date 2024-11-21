7b0a34c85298305f8f85bae645092e576e842143
Nojus <78691849+NoahIT@users.noreply.github.com>
2024-11-21 00:01:15 +0000

    Projects page additions

---
+++ Scripts/Projects/OnLoad.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    document.querySelectorAll('.animate-on-scroll').forEach(element => {
+        observer.observe(element);
+    });
+});
